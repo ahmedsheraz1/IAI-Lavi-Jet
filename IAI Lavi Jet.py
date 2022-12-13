83dab57ca2ce6e7bc77f6863f862d3b655d9491d
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 01:26:59 +0500

    Create IAI Lavi Jet.py

---
+++ IAI Lavi Jet.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'IAI Lavi Jet').hexdigest()
+'74125871741f9fc8a7eb12ca765d7065cba4066209d517f2d5027f12605c23943a969156dc233b3918779a6b05d3353b9324e6b3901e85853609d160a267c05f'
